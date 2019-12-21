c5d08b020aff51fb4dfbf58623b0b0a6e84c12d1
Paulo Camacho <paulo.ccamacho@gmail.com>
2019-12-20 16:45:19 -0800

    lets add htmlRoutes shall we

---
+++ app/data/friends.js
@@ -0,0 +1,82 @@
+var friends = [
+    {
+        name: "Annie",
+        photo: "https://www.tvovermind.com/wp-content/uploads/2017/12/erin-moriarty-joins-amazons-the-boys-640x427.jpg",
+        scores: [
+            '5',
+            '4',
+            '5',
+            '5',
+            '2',
+            '5',
+            '3',
+            '5',
+            '1',
+            '3'
+        ]
+    },
+    {
+        name: "Hughie",
+        photo: "https://www.cheatsheet.com/wp-content/uploads/2019/07/jack-quaid-1024x683.jpg",
+        scores: [
+            '2',
+            '3',
+            '2',
+            '5',
+            '5',
+            '5',
+            '3',
+            '1',
+            '2',
+            '1'
+        ]
+    },
+    {
+        name: "Billy",
+        photo: "https://img.purch.com/o/aHR0cDovL3d3dy5uZXdzYXJhbWEuY29tL2ltYWdlcy9pLzAwMC8yMjQvMjI0L2kwMi9pbWFnZTAwMS5qcGc=",
+        scores: [
+            '1',
+            '2',
+            '3',
+            '4',
+            '1',
+            '5',
+            '2',
+            '5',
+            '4',
+            '4'
+        ]
+    },
+    {
+        name: "John",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDcQXHAv_EeUBbHb1iHQ1SCBSgVr607zrOCYUFNQoo7IZ6EyS-Hg&s",
+        scores: [
+            '1',
+            '1',
+            '5',
+            '1',
+            '4',
+            '2',
+            '1',
+            '4',
+            '2',
+            '5'
+        ]
+    },
+    {
+        name: "Becca",
+        photo: "https://biographicsworld.com/wp-content/uploads/2019/04/shantel-vansenten-1024x592.jpeg",
+        scores: [
+            '3',
+            '3',
+            '4',
+            '3',
+            '2',
+            '5',
+            '5',
+            '2',
+            '3',
+            '2'
+        ]
+    }
+]
---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+
+var app = express();
+var PORT = 3000;
+
+
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+})
